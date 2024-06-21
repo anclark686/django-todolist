2849c1bec0305cf1c94151ce0cf7487603961edf
anclark686 <anclark686@gmail.com>
2024-06-21 04:53:30 -0400

    adding staticfiles

--- todolist/settings.py
+++ todolist/settings.py
@@ -122,10 +122,9 @@ STATIC_URL = 'static/'
 
 # STATIC_ROOT = 
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
